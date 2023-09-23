f9edef1fa527488daf7912347f65554f28067250
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-22 23:38:11 -0300

    refactor: Updating tab size

--- src/App.jsx
+++ src/App.jsx
@@ -8,56 +8,51 @@ import { WeatherTypes } from "./modules/WeatherTypes";
 const APIKey = import.meta.env.VITE_API_KEY;
 
 export default function App() {
-	const [apiData, setApiData] = useState(null);
-	const [showWeather, setShowWeather] = useState(null);
-	const [loading, setLoading] = useState(false);
+  const [apiData, setApiData] = useState(null);
+  const [showWeather, setShowWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	const inputRef = useRef(null);
+  const inputRef = useRef(null);
 
-	const fetchWeather = async () => {
-		setLoading(true);
+  const fetchWeather = async () => {
+    setLoading(true);
 
-		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${APIKey}&lang=pt_br`)
-			.then((res) => res.json())
-			.then((data) => {
-				setApiData(null);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${APIKey}&lang=pt_br`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setApiData(null);
 
-				if (data.cod == 404 || data.cod == 400) {
-					setShowWeather([
-						{
-							type: "Local não encontrado",
-							img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
-						},
-					]);
-				}
+        if (data.cod == 404 || data.cod == 400) {
+          setShowWeather([
+            {
+              type: "Local não encontrado",
+              img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+            },
+          ]);
+        }
 
-				setShowWeather(
-					WeatherTypes.filter(
-						(weather) => weather.type === data.weather[0].main
-					)
-				);
+        setShowWeather(
+          WeatherTypes.filter(
+            (weather) => weather.type === data.weather[0].main
+          )
+        );
 
-				setApiData(data);
-				setLoading(false);
-			})
-			.catch(() => {
-				setLoading(false);
-			});
-	};
+        setApiData(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
 
-	return (
-		<main className="grid h-screen place-items-center">
-			<section className="w-full max-w-sm p-4 rounded-2xl bg-input">
-				<Input
-					inputRef={inputRef}
-					fetchWeather={fetchWeather}
-				/>
-				<Info
-					showWeather={showWeather}
-					loading={loading}
-					apiData={apiData}
-				/>
-			</section>
-		</main>
-	);
+  return (
+    <main className="grid h-screen place-items-center">
+      <section className="w-full max-w-sm p-4 rounded-2xl bg-input">
+        <Input inputRef={inputRef} fetchWeather={fetchWeather} />
+        <Info showWeather={showWeather} loading={loading} apiData={apiData} />
+      </section>
+    </main>
+  );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,34 +1,31 @@
 import { MagnifyingGlass } from "phosphor-react";
 
-export default function Input({
-	inputRef,
-	fetchWeather
-}) {
-	const handleKeyPress = (event) => {
-		if (event.key === "Enter") {
-			fetchWeather();
-		}
-	}
+export default function Input({ inputRef, fetchWeather }) {
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      fetchWeather();
+    }
+  };
 
-	return (
-		<div className="flex items-center justify-between">
-			<input
-				type="text"
-				ref={inputRef}
-				onKeyDown={handleKeyPress}
-				placeholder="Insira o nome do local"
-				className="p-1 text-xl font-semibold text-textSecondary bg-input placeholder:text-textSecondary focus:outline-none"
-			/>
-			<button
-				onClick={fetchWeather}
-				className="duration-300 ease active:scale-90"
-			>
-				<MagnifyingGlass
-					className="text-textSecondary"
-					weight="bold"
-					size={30}
-				/>
-			</button>
-		</div>
-	)
+  return (
+    <div className="flex items-center justify-between">
+      <input
+        type="text"
+        ref={inputRef}
+        onKeyDown={handleKeyPress}
+        placeholder="Insira o nome do local"
+        className="p-1 text-xl font-semibold text-textSecondary bg-input placeholder:text-textSecondary focus:outline-none"
+      />
+      <button
+        onClick={fetchWeather}
+        className="duration-300 ease active:scale-90"
+      >
+        <MagnifyingGlass
+          className="text-textSecondary"
+          weight="bold"
+          size={30}
+        />
+      </button>
+    </div>
+  );
 }
